05a18580578d085dd8996b62f4ec6e83227f3ea2
Evgeny <djek.uait1998@gmail.com>
2023-06-06 00:34:17 +0300

    third commit

---
+++ src/blocks/foto/foto.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".foto__items").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    })
+})
