440209dcb2eac7ca094a32638692d21a8c9847ff
Evgeny <djek.uait1998@gmail.com>
2023-06-15 22:21:19 +0300

    some commit

---
+++ src/blocks/foto4/foto4.js
@@ -0,0 +1,27 @@
+new Swiper ('.foto4__slider', {
+    // Optional parameters
+    slidesPerView: 1,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
+  
+    spaceBetween: 10,
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+  });
