2ea8bb3425d72197c9a30c76d93e8cac2238e292
Evgeny <djek.uait1998@gmail.com>
2023-07-07 01:33:43 +0300

    some commit

--- src/blocks/master/master.js
+++ src/blocks/master/master.js
@@ -1,14 +1,11 @@
-$(document).ready(function(){
-    $(".master__items").slick({
-        //prevArrow:".more__button-prev",
-        //nextArrow:".more__button-next",
-        vertical: true,
-        verticalSwiping: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-        arrows: false,
-        
-    })
+new Swiper ('.master__slider', {
+    direction: "vertical",
+    slidesPerView: 1,
+    loop: true,
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
 })
