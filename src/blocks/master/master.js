1e0b47a94487f88f6ce4260cc41a6630ba0fa8a7
Evgeny <djek.uait1998@gmail.com>
2023-07-10 23:22:10 +0300

    fucking swiper

--- src/blocks/master/master.js
+++ src/blocks/master/master.js
@@ -1,11 +1,41 @@
 new Swiper ('.master__slider', {
-    direction: "vertical",
+    direction: getDirection(),
     slidesPerView: 1,
     loop: true,
-    
+    spaceBetween: 10,
+    autoplay: {
+        delay: 4000,
+    },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 
-})
+    on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        }
+    }
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 768 ? 'horizontal' : 'vertical';
+
+    return direction;
+}
+
+
+/*breakpoints: {
+        
+    320: {
+        direction: "horizontal",
+        slidesPerView: 1,
+      
+    },
+    
+    /*1000: {
+        direction: "horizontal",
+        slidesPerView: 1,
+       
+    },*/
